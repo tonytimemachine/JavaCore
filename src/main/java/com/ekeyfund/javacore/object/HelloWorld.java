361db1ed8b6e36439c6802feb611a96b7e8ff4c1
liuguangei@ekeyfund.com <tonytimemachine@gmail.com>
2017-07-14 18:15:36 +0800

    add third party commons  package HelloWorld Application

---
+++ src/main/java/com/ekeyfund/javacore/object/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.ekeyfund.javacore.object;
+
+/**
+ * Hello World Application
+ *
+ * @author tony liuguanglei@ekeyfund.com
+ * @create 2017-07-14-下午5:49
+ * @see
+ * @since JDK1.8u133
+ */
+public class HelloWorld {
+
+
+    public static void main(String[]args){
+
+        System.out.println("Hello World ");
+    }
+}
